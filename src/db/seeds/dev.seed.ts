5d334872870d934c46906ca42b943c58b04dee66
roremDev <rorem.94.developer@gmail.com>
2022-05-07 15:12:34 -0500

    feat: ✨ added bcrypt dependency inside of seed

--- src/db/seeds/dev.seed.ts
+++ src/db/seeds/dev.seed.ts
@@ -1,4 +1,5 @@
 import 'module-alias/register'
+import bcrypt from 'bcrypt'
 import { PrismaClient } from '@prisma/client'
 
 import posts from '@db/mocks/post.mock'
@@ -24,9 +25,27 @@ async function createBatch<TModel>(name: string, collection: Array<TModel>) {
     info(`${name} created`)
 }
 
+/**
+ * @description Create a new batch of user records with encrypted password
+ * in the database.
+ * @param {Array} collection Collection of records to be created. */
+async function createUserBatch(collection: Array<IUser>) {
+    await Promise.all(
+        collection.map(async ({ password, ...user }) => {
+            await _prisma.user.create({
+                data: {
+                    ...user,
+                    password: await bcrypt.hash(password, 10),
+                },
+            })
+        })
+    ).catch(({ message }) => fail(message))
+    info(`user created`)
+}
+
 ;(async () => {
     await createBatch<IPost>('post', posts)
-    await createBatch<IUser>('user', users)
+    await createUserBatch(users)
 })()
     .then(() => {
         _prisma.$disconnect()
