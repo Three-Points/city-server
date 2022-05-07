8ed0461528ae84a4b7b202f66ab6b7913dd6ba8c
roremDev <rorem.94.developer@gmail.com>
2022-05-07 14:17:51 -0500

    feat: ✨ added user model layer

---
+++ src/models/User/User.entity.ts
@@ -0,0 +1,23 @@
+export interface IUser {
+    id: number
+    name: string
+    email: string
+    password: string
+    bio: string
+    active: boolean
+    role: string | 'ADMIN' | 'USER'
+    createdAt?: Date | string
+    updatedAt?: Date | string
+}
+
+export type TQUser = {
+    id?: number
+    page?: number
+}
+export type TPUser = {
+    name?: string
+    email?: string
+    password?: string
+    bio?: string
+    role?: string | 'ADMIN' | 'USER'
+}
